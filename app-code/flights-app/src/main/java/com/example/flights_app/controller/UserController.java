428c54065c6852961cec44324e4fe33d403c6295
katyyy111 <kasiakuzora@gmail.com>
2026-06-13 13:32:35 +0200

    Added a users enpoint

---
+++ app-code/flights-app/src/main/java/com/example/flights_app/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.flights_app.controller;
+
+import com.example.flights_app.model.User;
+import com.example.flights_app.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
